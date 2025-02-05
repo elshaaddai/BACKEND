57bc8c4a120e52bd0065b6d9f38e0421e892a7a4
elshaddai tambuwun <elshatambuwun17@gmail.com>
2025-02-05 09:13:23 +0700

    last commit exercise #02

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 const http = require('http');
-const {hello,greetings} = require('./helloWorld');
-const moment = require('moment')
+const moment = require('moment');
+const users = require('./users');
 
 const server = http.createServer( (req,res) => {
-        // res.write(hello)
-        // res.write(greetings())
     const url = req.url;
-    if (url === '/users'){
+    if (url === '/'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type','text/json');
+        res.write('This is the home page')
+    }
+    else if (url === '/about'){
         res.statusCode = 200;
         res.setHeader('Content-Type','text/json');
         res.write(JSON.stringify({
             status:'success',
-            message:'users',
+            message:'response success',
+            description: 'exercise #02',
             date: moment().format(' MMMM Do YYYY, h:mm:ss a')
         }))
     }
-    else if (url === '/post'){
+    else if (url === '/users'){
         res.statusCode = 200;
         res.setHeader('Content-Type','text/json');
-        res.write(JSON.stringify({
-            status:'success',
-            message:'post',
-            date: moment().format(' MMMM Do YYYY, h:mm:ss a')
-        }))
+        res.write(JSON.stringify(users))
     }
-    else {
+    else{
         res.statusCode = 404;
-        res.setHeader('Content-Type','text/json');
-        res.write(JSON.stringify({
-            status:'Not Found',
-            message:'Tidak Ditemukan',
-            date: moment().format(' MMMM Do YYYY, h:mm:ss a')
-        }))
+        res.setHeader('Content-Type','text/json')
+        res.write('404 Users Not Found');
     }
     
     res.end();
@@ -42,3 +38,55 @@ const port = 3000;
 server.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}`);
 });
+
+
+
+
+
+
+
+
+// const http = require('http');
+// const {hello,greetings} = require('./helloWorld');
+// const moment = require('moment')
+
+// const server = http.createServer( (req,res) => {
+//         // res.write(hello)
+//         // res.write(greetings())
+//     const url = req.url;
+//     if (url === '/users'){
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type','text/json');
+//         res.write(JSON.stringify({
+//             status:'success',
+//             message:'users',
+//             date: moment().format(' MMMM Do YYYY, h:mm:ss a')
+//         }))
+//     }
+//     else if (url === '/post'){
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type','text/json');
+//         res.write(JSON.stringify({
+//             status:'success',
+//             message:'post',
+//             date: moment().format(' MMMM Do YYYY, h:mm:ss a')
+//         }))
+//     }
+//     else {
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type','text/json');
+//         res.write(JSON.stringify({
+//             status:'Not Found',
+//             message:'Tidak Ditemukan',
+//             date: moment().format(' MMMM Do YYYY, h:mm:ss a')
+//         }))
+//     }
+    
+//     res.end();
+// })
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// server.listen(port, hostname, () => {
+//     console.log(`server running at http://${hostname}:${port}`);
+// });
